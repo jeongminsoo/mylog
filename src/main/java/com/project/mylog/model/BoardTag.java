188b4f8ac07da8eebec8b87add73f3bee2851be5
Sharon-l-Lee <euniq20@gmail.com>
2022-08-01 10:59:50 +0900

    db_sqlupload

---
+++ src/main/java/com/project/mylog/model/BoardTag.java
@@ -0,0 +1,10 @@
+package com.project.mylog.model;
+
+import lombok.Data;
+
+@Data
+public class BoardTag {
+
+	private int hno;
+	private int rnum;
+}
---
+++ src/main/java/com/project/mylog/model/Timer.java
@@ -0,0 +1,13 @@
+package com.project.mylog.model;
+
+public class Timer {
+	
+	private int tno;
+	private String mid;
+	private String mname;
+	private String tname;
+	private int tstart;
+	private int tend;
+
+
+}
